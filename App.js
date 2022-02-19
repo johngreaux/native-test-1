db3de512fd8c84e2ef92261817615a289462b184
John Greaux <john.greaux@gmail.com>
2022-02-19 15:29:51 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Board } from 'react-native-draganddrop-board'
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Board
+        boardRepository={boardRepository}
+        open={() => {}}
+        onDragEnd={() => {}}
+      />
       <StatusBar style="auto" />
     </View>
   );
